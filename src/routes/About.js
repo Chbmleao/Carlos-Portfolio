1444a6731076c77eec37a23569404df5e8469278
Carlos Leão <caikleao@gmail.com>
2023-05-29 23:21:00 -0300

    Add hero image to about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="SOBRE" text="Algumas informações sobre mim" />
       <Footer />
     </div>
   );
