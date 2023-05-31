aaf8ab052566b305cb442a0fa0cebdfcecafd266
Carlos Leão <caikleao@gmail.com>
2023-05-31 10:43:18 -0300

    Add work section to project page

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
+import Work from "../components/Work";
 
 const Project = () => {
   return (
@@ -11,6 +12,7 @@ const Project = () => {
         heading="PROJETOS"
         text="Alguns dos meus trabalhos mais recentes"
       />
+      <Work />
       <Footer />
     </div>
   );
