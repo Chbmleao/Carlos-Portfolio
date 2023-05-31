c5be7bc0b1367e959f066f7c34fdd190e59043ac
Carlos Leão <caikleao@gmail.com>
2023-05-31 14:11:32 -0300

    Add project 2 data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,5 @@
 import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
 
 const ProjectCardData = [
   {
@@ -9,11 +10,11 @@ const ProjectCardData = [
     source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
   },
   {
-    imgsrc: project1,
-    title: "Visualizador de Algoritmos de Ordenação",
-    text: "Visualizador desenvolvido em Python para diversos algoritmos de ordenação. Entre esses, estão disponíveis: Selection sort, Insertion sort, Bubble sort, Quick sort, Merge Sort e Heap Sort. A aplicação também permite alterar a ordenação entre ascendente ou descendente, assim como o tamanho da lista e a velocidade da ordenação.",
+    imgsrc: project2,
+    title: "Acad Game",
+    text: "Acad Game é uma plataforma gameficada focada no público que frequenta academias com o intuito de competir com outras pessoas, compartilhar o progresso e persistir na evolução por meio do incentivo de conquistas. Aplicação em JavaScript e TypeScript que adota uma Arquitetura Hexagonal, detalhada na documentação presente no repositório Github, desenvolvido por cinco estudantes da UFMG.",
     view: "",
-    source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
+    source: "https://github.com/Chbmleao/AcadGame",
   },
   {
     imgsrc: project1,
