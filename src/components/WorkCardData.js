c60eaf4fae628537580f7cf91cbb9779cd3375cf
Carlos Leão <caikleao@gmail.com>
2023-05-31 10:42:15 -0300

    Add work card data structure

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,27 @@
+import project1 from "../assets/project1.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: project1,
+    title: "Visualizador de Algoritmos de Ordenação",
+    text: "Visualizador desenvolvido em Python para diversos algoritmos de ordenação. Entre esses, estão disponíveis: Selection sort, Insertion sort, Bubble sort, Quick sort, Merge Sort e Heap Sort. A aplicação também permite alterar a ordenação entre ascendente ou descendente, assim como o tamanho da lista e a velocidade da ordenação.",
+    view: "",
+    source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
+  },
+  {
+    imgsrc: project1,
+    title: "Visualizador de Algoritmos de Ordenação",
+    text: "Visualizador desenvolvido em Python para diversos algoritmos de ordenação. Entre esses, estão disponíveis: Selection sort, Insertion sort, Bubble sort, Quick sort, Merge Sort e Heap Sort. A aplicação também permite alterar a ordenação entre ascendente ou descendente, assim como o tamanho da lista e a velocidade da ordenação.",
+    view: "",
+    source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
+  },
+  {
+    imgsrc: project1,
+    title: "Visualizador de Algoritmos de Ordenação",
+    text: "Visualizador desenvolvido em Python para diversos algoritmos de ordenação. Entre esses, estão disponíveis: Selection sort, Insertion sort, Bubble sort, Quick sort, Merge Sort e Heap Sort. A aplicação também permite alterar a ordenação entre ascendente ou descendente, assim como o tamanho da lista e a velocidade da ordenação.",
+    view: "",
+    source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
+  },
+];
+
+export default ProjectCardData;
