4c67c2e86f38248304f601c59c3cdb77198d1998
Carlos Leão <caikleao@gmail.com>
2023-06-01 22:27:34 -0300

    Change form component structure to support the popup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,77 +1,96 @@
-import React, { useRef } from "react";
+import React, { Component } from "react";
 import "./FormStyles.css";
 import emailjs from "@emailjs/browser";
+import Popup from "./Popup";
 
-const Form = () => {
-  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-  const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
-
-  const form = useRef();
-
-  const userNameRef = useRef(null);
-  const userEmailRef = useRef(null);
-  const subjectRef = useRef(null);
-  const messageRef = useRef(null);
-
-  const handleClearFields = () => {
-    userNameRef.current.value = "";
-    userEmailRef.current.value = "";
-    subjectRef.current.value = "";
-    messageRef.current.value = "";
+class Form extends Component {
+  state = {
+    userName: "",
+    userEmail: "",
+    subject: "",
+    message: "",
   };
 
-  const sendEmail = (e) => {
+  popupRef = React.createRef();
+  formRef = React.createRef();
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  resetInputValues = () => {
+    this.setState({
+      userName: "",
+      userEmail: "",
+      subject: "",
+      message: "",
+    });
+  };
+
+  sendEmail = (e) => {
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
     e.preventDefault();
 
-    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
-
-    handleClearFields();
+    emailjs
+      .sendForm(serviceId, templateId, this.formRef.current, publicKey)
+      .then(
+        (result) => {
+          console.log(result.text);
+          this.popupRef.current.toggleClassName();
+          this.resetInputValues();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
-  return (
-    <div className="form">
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Seu nome</label>
-        <input
-          className="text-box"
-          type="text"
-          name="user_name"
-          ref={userNameRef}
-        ></input>
-        <label>Email</label>
-        <input
-          className="text-box"
-          type="text"
-          name="user_email"
-          ref={userEmailRef}
-        ></input>
-        <label>Assunto</label>
-        <input
-          className="text-box"
-          type="text"
-          name="subject"
-          ref={subjectRef}
-        ></input>
-        <label>Mensagem</label>
-        <textarea
-          className="text-box"
-          rows="6"
-          placeholder="Digite sua mensagem aqui!"
-          name="message"
-          ref={messageRef}
-        ></textarea>
-        <input className="btn" type="submit" value="Send" />
-      </form>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="form">
+        <form ref={this.formRef} onSubmit={this.sendEmail}>
+          <label>Seu nome</label>
+          <input
+            className="text-box"
+            type="text"
+            name="userName"
+            value={this.state.userName}
+            onChange={this.handleInputChange}
+          ></input>
+          <label>Email</label>
+          <input
+            className="text-box"
+            type="text"
+            name="userEmail"
+            value={this.state.userEmail}
+            onChange={this.handleInputChange}
+          ></input>
+          <label>Assunto</label>
+          <input
+            className="text-box"
+            type="text"
+            name="subject"
+            value={this.state.subject}
+            onChange={this.handleInputChange}
+          ></input>
+          <label>Mensagem</label>
+          <textarea
+            className="text-box"
+            rows="6"
+            placeholder="Digite sua mensagem aqui!"
+            name="message"
+            value={this.state.message}
+            onChange={this.handleInputChange}
+          ></textarea>
+          <input className="btn" type="submit" value="Send" />
+        </form>
+        <Popup ref={this.popupRef} />
+      </div>
+    );
+  }
+}
 
 export default Form;
