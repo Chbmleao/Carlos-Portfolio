42f0b70d72916f99ac16b89a5f508d0051a1c9b5
Carlos Leão <caikleao@gmail.com>
2023-06-01 22:26:40 -0300

    Add send email popup component

---
+++ src/components/Popup.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { AiOutlineCheck } from "react-icons/ai";
+import "./PopupStyles.css";
+
+class Popup extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleClassName = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <div className={`popup ${isOpen ? "open-popup" : ""}`} id="popup">
+        <AiOutlineCheck className="check" />
+        <h2>Obrigado!</h2>
+        <p>Obrigado pelo contato. Sua mensagem foi enviada com sucesso.</p>
+        <button className="btn" type="button" onClick={this.toggleClassName}>
+          OK
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Popup;
