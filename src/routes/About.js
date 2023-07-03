6b412fdf6f32c0fa6598510c771fc96a04dd3fab
Carlos Leão <caikleao@gmail.com>
2023-07-03 19:31:42 -0300

    Add AboutMe section to About page

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
+import AboutMe from "../components/AboutMe";
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="SOBRE" text="Algumas informações sobre mim" />
+      <AboutMe />
       <Footer />
     </div>
   );
