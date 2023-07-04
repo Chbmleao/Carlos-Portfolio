f4a58e5207c0966ccdb8b13d4300ed7f98cb13e2
Carlos Leão <caikleao@gmail.com>
2023-07-04 16:43:34 -0300

    Add ScrollButton component

---
+++ src/components/ScrollButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ScrollButtonStyles.css";
+
+class ScrollButton extends React.Component {
+  scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+
+  render() {
+    const { sectionId, sectionName } = this.props;
+    return (
+      <button onClick={() => this.scrollToSection(sectionId)}>
+        {sectionName}
+      </button>
+    );
+  }
+}
+
+export default ScrollButton;
