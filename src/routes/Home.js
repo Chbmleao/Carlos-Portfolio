08e6d92c6b15a59f956f858637c8f48f5566bfe1
Carlos Leão <caikleao@gmail.com>
2023-07-04 16:47:09 -0300

    Add another sections to main page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,13 +3,19 @@ import Navbar from "../components/Navbar";
 import HeroImg from "../components/HeroImg";
 import Footer from "../components/Footer";
 import Work from "../components/Work";
+import AboutMe from "../components/AboutMe";
+import Timeline from "../components/Timeline";
+import Form from "../components/Form";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <AboutMe />
+      <Timeline />
       <Work />
+      <Form />
       <Footer />
     </div>
   );
