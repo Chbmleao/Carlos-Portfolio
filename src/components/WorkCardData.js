0624168be539e3e53fbeb03f548c293dbbd4795b
Carlos Leão <caikleao@gmail.com>
2023-08-05 13:58:44 -0300

    Add new work card data for project 4

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
+import project4 from "../assets/project4.gif";
 
 const ProjectCardData = [
   {
@@ -14,6 +15,12 @@ const ProjectCardData = [
     title: "Acad Game",
     text: "Acad Game é uma plataforma gameficada focada no público que frequenta academias com o intuito de competir com outras pessoas, compartilhar o progresso e persistir na evolução por meio do incentivo de conquistas. Aplicação em JavaScript e TypeScript que adota uma Arquitetura Hexagonal, detalhada na documentação presente no repositório Github, desenvolvido por cinco estudantes da UFMG.",
     source: "https://github.com/Chbmleao/AcadGame",
+  },
+  {
+    imgsrc: project4,
+    title: "Pathfinding Visualizer",
+    text: "Este projeto fornece uma interface para visualizar a funcionalidade de vários algoritmos de caminhamento em grafos. Um algoritmo de busca de caminho é construído com base em um algoritmo de busca de grafo e explora rotas entre nós, começando em um nó e percorrendo relacionamentos até que o destino seja alcançado, como os algoritmos DFS, BFS e Dijkstra",
+    source: "https://pathfinding-visualizer-carlosleao.vercel.app/",
   },
   {
     imgsrc: project3,
