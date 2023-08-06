b288e28f54fa21ca413dd91adc7f3b33f7ec81b4
Carlos Leão <caikleao@gmail.com>
2023-08-06 18:37:33 -0300

    Add new flip card component

---
+++ src/components/FlipCard.js
@@ -0,0 +1,19 @@
+import "../styles/FlipCardStyles.css";
+import React from "react";
+
+const FlipCard = ({ imgsrc1, imgsrc2 }) => {
+  return (
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img src={imgsrc1}></img>
+        </div>
+        <div className="flip-card-back">
+          <img src={imgsrc2}></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipCard;
