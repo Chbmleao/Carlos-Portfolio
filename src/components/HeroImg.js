2b7b3747d19861fceda59adf709bac951e519bbc
Carlos Leão <caikleao@gmail.com>
2023-08-14 13:58:19 -0300

    Add TypeWriter to hero image section

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,9 +1,17 @@
 import "../styles/HeroImgStyles.css";
 import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import IntroImg from "../assets/hero-bg.jpg";
 import ScrollButton from "./ScrollButton";
 
 const HeroImg = () => {
+  const [text] = useTypewriter({
+    words: ["Front-end", "Back-end", "Banco de dados", "Desenvolvedor"],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 80,
+  })
+
   return (
     <div className="hero" id="home">
       <div className="mask">
@@ -11,7 +19,14 @@ const HeroImg = () => {
       </div>
       <div className="content">
         <p>OLÁ, ME CHAMO CARLOS LEÃO</p>
-        <h1>Desenvolvedor</h1>
+        <h1>
+          <span>
+            {text}
+          </span>
+          <span>
+            <Cursor cursorStyle="|"/>
+          </span>
+        </h1>
         <div>
           <ScrollButton
             buttonType="btn"
