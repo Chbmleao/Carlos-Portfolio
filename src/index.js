58b5d8a6cdafd28bb1e2b7820070e2e2e3a8928d
Carlos Leão <caikleao@gmail.com>
2023-08-17 21:07:09 -0300

    Add i18next to the project to use translations to portuguese and english

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import ptTranslation from "./translations/pt-br/global.json";
+import enTranslation from "./translations/en/global.json";
+import i18next from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+
+i18next.use(initReactI18next).init({
+  resources: {
+    pt: {
+      translation: ptTranslation,
+    },
+    en: {
+      translation: enTranslation,
+    },
+  },
+  lng: "pt",
+  fallbackLng: "pt",
+  interpolation: { escapeValue: false },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <I18nextProvider i18n={i18next} />
     <App />
   </BrowserRouter>
 );
