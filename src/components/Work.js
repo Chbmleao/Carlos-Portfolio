2f2f609182363586b8098ba5d88b8209717975b8
Carlos Leão <caikleao@gmail.com>
2023-08-17 22:40:26 -0300

    Add i18n translation

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,18 +4,24 @@ import WorkCardData from "./WorkCardData";
 
 import React from "react";
 
+import { useTranslation } from "react-i18next";
+
 const Work = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="work-container" id="project">
-      <h1 className="project-heading">Meus Projetos</h1>
+      <h1 className="project-heading">{t("work.h1")}</h1>
       <div className="project-container">
         {WorkCardData.map((val, ind) => {
+          const translationText = "work.workData" + (ind + 1) + ".";
+
           return (
             <WorkCard
               key={ind}
               imgsrc={val.imgsrc}
-              title={val.title}
-              text={val.text}
+              title={t(translationText + "title")}
+              text={t(translationText + "text")}
               view={val.view}
               source={val.source}
             />
