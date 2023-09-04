aa329936f78d6dfa5c0e37c86559521ed923e0b9
Carlos Leão <caikleao@gmail.com>
2023-09-04 11:46:58 -0300

    Remove unnecessary data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,26 +7,19 @@ import project5 from "../assets/project5.png";
 const ProjectCardData = [
   {
     imgsrc: project1,
-    title: "Visualizador de Algoritmos de Ordenação",
-    text: "Visualizador desenvolvido em Python para diversos algoritmos de ordenação. Entre esses, estão disponíveis: Selection sort, Insertion sort, Bubble sort, Quick sort, Merge Sort e Heap Sort. A aplicação também permite alterar a ordenação entre ascendente ou descendente, assim como o tamanho da lista e a velocidade da ordenação.",
     source: "https://github.com/Chbmleao/Sorting-Algorithms-Visualizer",
   },
   {
     imgsrc: project2,
     title: "Acad Game",
-    text: "Acad Game é uma plataforma gameficada focada no público que frequenta academias com o intuito de competir com outras pessoas, compartilhar o progresso e persistir na evolução por meio do incentivo de conquistas. Aplicação em JavaScript e TypeScript que adota uma Arquitetura Hexagonal, detalhada na documentação presente no repositório Github, desenvolvido por cinco estudantes da UFMG.",
     source: "https://github.com/Chbmleao/AcadGame",
   },
   {
     imgsrc: project4,
-    title: "Pathfinding Visualizer",
-    text: "Este projeto fornece uma interface para visualizar a funcionalidade de vários algoritmos de caminhamento em grafos. Um algoritmo de busca de caminho é construído com base em um algoritmo de busca de grafo e explora rotas entre nós, começando em um nó e percorrendo relacionamentos até que o destino seja alcançado. O projeto foi desenvolvido em React e contém os algoritmos DFS, BFS e Dijkstra.",
     source: "https://pathfinding-visualizer-carlosleao.vercel.app/",
   },
   {
     imgsrc: project3,
-    title: "Final Fantasy Remake",
-    text: "Jogo desenvolvido em C juntamente com a biblioteca de desenvolvimento de jogos Allegro, durante meu primeiro contato com a linguagem, no primeiro semestre de 2021. O jogo é inspirado em jogos RPG, principalmente Final Fantasy e Pokemon. O projeto também apresenta uma documentação explicativa do código desenvolvido.",
     source: "https://github.com/Chbmleao/FinalFantasy-Remake",
   },
   {
